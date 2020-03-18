68774398fa853e86bc19ff50b8e12d32c35084f8
DAGORNE <47077580+DamsRock@users.noreply.github.com>
2020-03-18 14:08:02 +0100

    upd

--- carlab.py
+++ carlab.py
@@ -3,7 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-csv2012 = pd.read_csv("12-BASECLMAJJUIN2012.csv" , sep=",")
-csv2013 = pd.read_csv("13-cl_JUIN_2013-complet3.csv" , sep=",")
-csv2014 = pd.read_csv("14-mars-2014-complete.csv" , sep=",")
-csv2015 = pd.read_csv("15-fic_etiq_edition_40-mars-2015.csv" , sep=",")
+#csv2012 = pd.read_csv("./2012.csv", sep=",")
+#csv2013 = pd.read_csv("./2013.csv", sep=",")
+#csv2014 = pd.read_csv("./2014.csv", sep=",")
+csv2015 = pd.read_csv("./2015.csv" , sep=",")
+
+var2015 = csv2015.shape
+print(var2015)
