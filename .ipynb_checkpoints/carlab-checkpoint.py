295e462bde43653fae175e948a7dce981d6f071f
DAGORNE <47077580+DamsRock@users.noreply.github.com>
2020-03-20 11:03:04 +0100

    upd

---
+++ .ipynb_checkpoints/carlab-checkpoint.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+xls2001 = pd.read_excel("./2001.xls")
+xls2002 = pd.read_excel("./2002.xls")
+xls2003 = pd.read_excel("./2003.xls")
+xls2004 = pd.read_excel("./2004.xls")
+xls2005 = pd.read_excel("./2005.xls")
+xls2006 = pd.read_excel("./2006.xls")
+xls2007 = pd.read_excel("./2007.xls")
+xls2008 = pd.read_excel("./2008.xls")
+xls2009 = pd.read_excel("./2009.xls")
+xls2010 = pd.read_excel("./2010.xls")
+xls2011 = pd.read_excel("./2011.xls")
+xls2012 = pd.read_excel("./2012.xls")
+xls2013 = pd.read_excel("./2013.xls")
+xls2014 = pd.read_excel("./2014.xls")
+xls2015 = pd.read_excel("./2015.xls")
+
+
+df2001 = xls2001.shape
+df2002 = xls2002.shape
+df2003 = xls2003.shape
+df2004 = xls2004.shape
+df2005 = xls2005.shape
+df2006 = xls2006.shape
+df2007 = xls2007.shape
+df2008 = xls2008.shape
+df2009 = xls2009.shape
+df2010 = xls2010.shape
+df2011 = xls2011.shape
+df2012 = xls2012.shape
+df2013 = xls2013.shape
+df2014 = xls2014.shape
+df2015 = xls2015.shape
+
+
+
+print(df2001)
+print(df2002)
+print(df2003)
+print(df2001)
+print(df2001)
+print(df2001)
+print(df2001)
+print(df2001)
+print(df2001)
+print(df2010)
+print(df2011)
+print(df2012)
+print(df2013)
+print(df2014)
+print(df2015)
--- carlab.py
+++ carlab.py
@@ -20,35 +20,4 @@ xls2013 = pd.read_excel("./2013.xls")
 xls2014 = pd.read_excel("./2014.xls")
 xls2015 = pd.read_excel("./2015.xls")
 
-df2001 = xls2001.shape
-df2002 = xls2002.shape
-df2003 = xls2003.shape
-df2004 = xls2004.shape
-df2005 = xls2005.shape
-df2006 = xls2006.shape
-df2007 = xls2007.shape
-df2008 = xls2008.shape
-df2009 = xls2009.shape
-df2010 = xls2010.shape
-df2011 = xls2011.shape
-df2012 = xls2012.shape
-df2013 = xls2013.shape
-df2014 = xls2014.shape
-df2015 = xls2015.shape
-
-
-print(df2001)
-print(df2002)
-print(df2003)
-print(df2001)
-print(df2001)
-print(df2001)
-print(df2001)
-print(df2001)
-print(df2001)
-print(df2010)
-print(df2011)
-print(df2012)
-print(df2013)
-print(df2014)
-print(df2015)
+xls2015.shape
